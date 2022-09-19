dcc1ccb3efcdf35c3209d7988bb3e1dd359eeaca
Sergen Cansız <sergencansiz@gmail.com>
2022-09-19 19:35:04 +0200

    creates validator and applies on WSClient message validator

--- packages/server/src/WSClient.ts
+++ packages/server/src/WSClient.ts
@@ -1,34 +1,67 @@
 import {WebSocketClient} from "./Server";
 import {randomUUID} from "crypto";
+import Validator from "./utils/validator";
+
+
+type WSClientErrorMessage = {
+    t: string,
+    m: string
+}
 
 export class WSClient {
 
     public id: string
     public client: WebSocketClient
 
+    private validator: Validator
+    private message_types: string[] = ["message", "file", "sound"]
+
     constructor(client: WebSocketClient) {
         this.id = randomUUID()
         this.client = client
+        this.validator = new Validator()
 
         // Register message handler for client
         this.client.on('message', this.onMessageHandler)
+        this.client.on('close', this.onDisconnectHandler)
     }
-
 
     /**
      * Handler for client each time receives a data from remote client
      * @param message {string}
      */
-    private onMessageHandler = (message: string) => {
-        console.log(message.toString())
+    private onMessageHandler = (message: Buffer) => {
+        let {status, data} = this.validator.validate(message)
+        if(!status){
+            if(typeof data === "string") this.send_error(data)
+            return
+        }
+
+        if(!(data.t in this.message_types)){
+            this.send_error('Unrecognized message type')
+        }
+
     }
 
     /**
      * Handler for client disconnect
      */
-    private onDisconnectHandler = () => {
+    private onDisconnectHandler = (event: number, reason: Buffer) => {
+        console.log('Client disconnected')
+        console.log(event, reason.toString())
+    }
+
+    private m = (event: string, ) => {
 
     }
 
+    private send = (message: Object | WSClientErrorMessage) => {
+        let m = JSON.stringify(message)
+        this.client.send(m)
+    }
+
+    private send_error = (message: string) => {
+        this.send({t: "error" , m: message})
+    }
 
 }
---
+++ packages/server/src/utils/validator.ts
@@ -0,0 +1,76 @@
+const messageDataTag: unique symbol = Symbol("MessageDataTag");
+const messageTag: unique symbol = Symbol("MessageTag");
+
+/**
+ * Type for data in the message
+ */
+type MessageData = {
+    m: string
+    o: any
+}
+
+/**
+ * Type for message
+ */
+type Message = {
+    t: string
+    d: MessageData
+}
+
+type ValidatorResult = {
+    status: boolean
+    data: Message
+}
+
+
+class Validator {
+
+
+    public validate = (data: Buffer) : ValidatorResult => {
+
+        let d
+        try {
+            d = this.validate_json(data)
+            d = this.validate_schema(d)
+        }catch (e: any){
+            return {status: false, data: e}
+        }
+
+        return {status: true, data: d}
+    }
+
+
+    /**
+     * Validate whether given buffer type is JSON serializable
+     * @param data {Buffer}
+     */
+    private validate_json = (data: Buffer) : Message => {
+
+        let d
+
+        try {
+            d = JSON.parse(data.toString())
+        }
+        catch (e) {
+            throw 'Received data is not JSON serializable'
+        }
+        return d
+    }
+
+    /**
+     * Validate whether data had valid format
+     * @param data {Message}
+     */
+    private validate_schema = (data: Message) => {
+
+        if('t' in data && typeof data.t === "string" &&
+            'd' in data && typeof data.d === "object"
+        ){
+            return data
+        }else{
+            throw 'Message data is not well formatted'
+        }
+    }
+}
+
+export default Validator
